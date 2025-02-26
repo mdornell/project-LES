d35d5e6b054d5b91139473bd865d4040543ae3eb
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-02-26 19:51:11 -0300

    feat: Criando API

---
+++ les-api/src/main/java/com/example/les_api/LesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.les_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesApiApplication.class, args);
+	}
+
+}
---
+++ les-api/src/test/java/com/example/les_api/LesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.les_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
