9fe6ddb03d31648e4933ad4a3e8f4ce8136b6175
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-03-28 17:55:48 -0300

    feat: adicionar repositório de usuário com método para busca por email

--- les-api/src/main/java/com/example/les_api/repository/UsuarioRepository.java
+++ les-api/src/main/java/com/example/les_api/repository/UsuarioRepository.java
@@ -2,6 +2,10 @@ package com.example.les_api.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.les_api.domain.usuario.Usuario;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(String email);
 }
