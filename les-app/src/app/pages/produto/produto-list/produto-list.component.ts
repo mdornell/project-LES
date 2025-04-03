f5d9b2b1e12ae6cd2a7487f0ea1d36757183d9f7
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-04-03 13:29:17 -0300

    feat: adicionar componentes de produto com templates e estilos

---
+++ les-app/src/app/pages/produto/produto-list/produto-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './produto-list.component.html',
+  styleUrl: './produto-list.component.scss'
+})
+export class ProdutoListComponent {
+
+}
