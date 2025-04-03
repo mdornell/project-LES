cb0bd6025d09f045666448e6335ab3f717d20b5e
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-04-03 16:18:36 -0300

    feat: adicionar rotas para produtos e refeições

--- les-app/src/app/app.routes.ts
+++ les-app/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { ClienteComponent } from './pages/cliente/cliente.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginClienteComponent } from './pages/login-cliente/login-cliente.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProdutoFormComponent } from './pages/produto/produto-form/produto-form.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { RefeicaoComponent } from './pages/refeicao/refeicao.component';
 import { SignUpComponent } from './pages/signup/signup.component';
 import { UsuariosFormComponent } from './pages/usuarios/usuarios-form/usuarios-form.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
@@ -23,18 +26,26 @@ export const routes: Routes = [
         component: HomeComponent,
         canActivate: [AuthGuard],
         children: [
+            // Home
             { path: '', component: WelcomeComponent },
 
             // Funcinarios
-            { path: 'user', component: UsuariosComponent },
-            { path: 'user/new', component: UsuariosFormComponent, resolve: { usuario: UsuarioResolver } },
-            { path: 'user/edit/:id', component: UsuariosFormComponent, resolve: { usuario: UsuarioResolver } },
+            { path: 'funcionario', component: UsuariosComponent },
+            { path: 'funcionario/new', component: UsuariosFormComponent, resolve: { usuario: UsuarioResolver } },
+            { path: 'funcionario/edit/:id', component: UsuariosFormComponent, resolve: { usuario: UsuarioResolver } },
 
             // Clientes
             { path: 'cliente', component: ClienteComponent },
             { path: 'cliente/new', component: ClienteFormComponent, resolve: { cliente: ClienteResolver } },
             { path: 'cliente/edit/:id', component: ClienteFormComponent, resolve: { cliente: ClienteResolver } },
 
+            // Produtos
+            { path: 'produto', component: ProdutoComponent },
+            { path: 'produto/new', component: ProdutoFormComponent },
+            { path: 'produto/edit/:id', component: ProdutoFormComponent },
+
+            // Refeições
+            { path: 'refeicao', component: RefeicaoComponent }
 
         ]
     },
