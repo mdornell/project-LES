f3505f227667574dd3cbb5157a8c64eb4062419b
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-04-04 18:07:42 -0300

    feat: adicionar campo 'codigoBarras' em Produto e ProdutoDTO, atualizar ProdutoService e formulário de produto

--- les-api/src/main/java/com/example/les_api/dto/ProdutoDTO.java
+++ les-api/src/main/java/com/example/les_api/dto/ProdutoDTO.java
@@ -16,6 +16,7 @@ public class ProdutoDTO {
     private Integer id;
 
     private String nome;
+    private String codigoBarras;
     private String descricao;
     private Double preco;
     private Integer quantidade;
@@ -24,6 +25,7 @@ public class ProdutoDTO {
     public ProdutoDTO(Produto produto) {
         this.id = produto.getId();
         this.nome = produto.getNome();
+        this.codigoBarras = produto.getCodigoBarras();
         this.descricao = produto.getDescricao();
         this.preco = produto.getPreco();
         this.quantidade = produto.getQuantidade();
