f5620e94276b6e39d7cb9edffc6c01926956d130
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-04-04 18:07:49 -0300

    feat: adicionar inicialização de usuário admin no aplicativo e refatorar filtro de segurança

--- les-api/src/main/java/com/example/les_api/LesApiApplication.java
+++ les-api/src/main/java/com/example/les_api/LesApiApplication.java
@@ -1,13 +1,31 @@
 package com.example.les_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.les_api.domain.funcionario.Funcionario;
+import com.example.les_api.repository.FuncionarioRepository;
 
 @SpringBootApplication
 public class LesApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LesApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initAdmin(FuncionarioRepository funcionarioRepository) {
+        return args -> {
+            Funcionario funcionario = new Funcionario();
+            funcionario.setNome("admin");
+            funcionario.setCargo("Admin");
+            funcionario.setEmail("admin@admin.com");
+            org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder passwordEncoder = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+            funcionario.setSenha(passwordEncoder.encode("admin123"));
+            funcionarioRepository.save(funcionario);
+        };
     }
 
 }
