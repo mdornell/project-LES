b3ba5679ae1e7f561293117d81d9e99b31ccab93
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2025-04-25 15:39:55 -0300

    refactor: remover importações não utilizadas no ClienteRepository

--- les-api/src/main/java/com/example/les_api/repository/ClienteRepository.java
+++ les-api/src/main/java/com/example/les_api/repository/ClienteRepository.java
@@ -2,9 +2,7 @@ package com.example.les_api.repository;
 
 import java.util.Optional;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.les_api.domain.cliente.Cliente;
