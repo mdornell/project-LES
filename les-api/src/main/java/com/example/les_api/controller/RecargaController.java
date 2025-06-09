4352ba75f89b89567873a308af92aad936a0ff87
Ronan Jardim <ronanjardim8@gmail.com>
2025-06-08 22:11:06 -0300

    feat: refatorar lógica de recarga para o serviço e remover código redundante do controlador

--- les-api/src/main/java/com/example/les_api/controller/RecargaController.java
+++ les-api/src/main/java/com/example/les_api/controller/RecargaController.java
@@ -1,44 +1,23 @@
 package com.example.les_api.controller;
 
-import java.sql.Timestamp;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.les_api.domain.cliente.Cliente;
-import com.example.les_api.domain.recarga.Recarga;
 import com.example.les_api.dto.RecargaDTO;
-import com.example.les_api.repository.ClienteRepository;
-import com.example.les_api.repository.RecargaRepository;
+import com.example.les_api.service.RecargaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/recargas")
 public class RecargaController {
 
-    private final RecargaRepository recargaRepository;
-    private final ClienteRepository clienteRepository;
+    private final RecargaService recargaService;
 
-    public RecargaController(RecargaRepository recargaRepository, ClienteRepository clienteRepository) {
-        this.recargaRepository = recargaRepository;
-        this.clienteRepository = clienteRepository;
+    public RecargaController(RecargaService recargaService) {
+        this.recargaService = recargaService;
     }
 
     @PostMapping
     public ResponseEntity<?> registrarRecarga(@RequestBody RecargaDTO dto) {
-        Cliente cliente = clienteRepository.findById(dto.getClienteId())
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-
-        cliente.setSaldo(cliente.getSaldo() + dto.getValor());
-        clienteRepository.save(cliente);
-        Recarga recarga = new Recarga();
-        recarga.setValor(dto.getValor());
-        recarga.setCliente(cliente);
-        recarga.setDataRecarga(Timestamp.valueOf(dto.getData())); // Formato: yyyy-MM-dd HH:mm:ss
-
-        recargaRepository.save(recarga);
+        recargaService.registrarRecarga(dto);
         return ResponseEntity.ok("Recarga registrada com sucesso.");
     }
 }
