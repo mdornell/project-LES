7b22ad35c54fdf22ab2077e92b716f02b1ae0951
Ronan Jardim <ronanjardim8@gmail.com>
2025-06-09 00:31:24 -0300

    feat: implementar CRUD para telas com mapeamento de URLs

--- les-api/src/main/java/com/example/les_api/repository/TelaRepository.java
+++ les-api/src/main/java/com/example/les_api/repository/TelaRepository.java
@@ -2,6 +2,8 @@ package com.example.les_api.repository;
 
 import com.example.les_api.domain.permissao.Tela;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TelaRepository extends JpaRepository<Tela, Integer> {
 }
