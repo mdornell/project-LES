75963b78465374c0c42869fbe7461c679673a358
Ronan Jardim <ronanjardim8@gmail.com>
2025-07-04 15:53:49 -0300

    feat: implementar funcionalidade para quitar dívidas de clientes e ajustar configuração de vendas

--- les-api/src/main/java/com/example/les_api/repository/VendaRepository.java
+++ les-api/src/main/java/com/example/les_api/repository/VendaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.les_api.domain.venda.Venda;
 
@@ -11,8 +12,6 @@ public interface VendaRepository extends JpaRepository<Venda, Integer> {
     @Query("SELECT DATE(v.dataHora), COUNT(DISTINCT v.cliente.id) FROM Venda v GROUP BY DATE(v.dataHora)")
     List<Object[]> clientesPorDia();
 
-    // @Query("SELECT DATE(v.dataHora), SUM(iv.custo), SUM(iv.preco) FROM Venda v
-    // JOIN v.itens iv GROUP BY DATE(v.dataHora)")
-    // List<Object[]> dreDiario();
-
+    @Query("SELECT v FROM Venda v WHERE v.cliente.id = :clienteId AND v.paga = false")
+    List<Venda> buscarVendasNaoPagasPorCliente(@Param("clienteId") Integer clienteId);
 }
